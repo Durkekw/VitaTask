d43be1de53471891489f2447b2b250707b57fa09
Durkekw <durkee0@mail.ru>
2025-03-07 12:40:21 +0300

    smthng

--- Backend/models/user.go
+++ Backend/models/user.go
@@ -1,10 +1,12 @@
 package models
 
+import "database/sql"
+
 type User struct {
-	UserID   int    `json:"user_id"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Name     string `json:"name"`
-	Surname  string `json:"surname"`
-	TeamID   int    `json:"team_id"`
+	UserID   int           `json:"user_id"`
+	Email    string        `json:"email"`
+	Password string        `json:"password"`
+	Name     string        `json:"name"`
+	Surname  string        `json:"surname"`
+	TeamID   sql.NullInt64 `json:"team_id"`
 }
